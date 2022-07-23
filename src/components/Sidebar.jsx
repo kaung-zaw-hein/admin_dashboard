97bf2777f51e62e51b2532b12e491b75e89b3ff8
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-07-23 13:42:51 +0630

    Navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,13 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
   
-  const { activeMenu, setActiveMenu } =  useStateContext();
+  const { currentColor, activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if(activeMenu !== undefined && screenSize <= 900){
+      setActiveMenu(false);
+    }
+  }
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-400 dark:hover:text-black hover:bg-light-gray m-2';
@@ -19,7 +25,7 @@ const Sidebar = () => {
       {activeMenu && (
         <>
           <div className="flex items-center justify-between">
-            <Link to="/" onClick={() => setActiveMenu(false)} className="flex items-center gap-3 mt-4 ml-3 text-xl font-extrabold tracking-tight text-slate-900 ">
+            <Link to="/" onClick={handleCloseSideBar} className="flex items-center gap-3 mt-4 ml-3 text-xl font-extrabold tracking-tight text-slate-900 ">
               <SiShopware /> <span>Shoppy</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
@@ -42,7 +48,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={{}}
+                    onClick={handleCloseSideBar}
                     style={({ isActive }) => ({
                       backgroundColor: isActive ? "blue" : '',
                     })}
