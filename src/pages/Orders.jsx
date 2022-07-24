6c037b45031b7283e48f0f76b8b6d89d5020a5a4
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-07-24 14:41:39 +0630

    Order table

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Orders = () => {
+  
+  const editing = { allowDeleting: true, allowEditing: true };
+
   return (
-    <div>Orders</div>
+    <div className="p-2 m-2 mt-24 bg-white md:m-10 md:p-10 rounded-3xl">
+      <Header category="Page"  title="Order" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
+        contextMenuItems={contextMenuItems}
+        editSettings={editing}
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item,index) => (
+            <ColumnDirective key={index} {...item}/>
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
+      </GridComponent>
+    </div>
   )
 }
 
