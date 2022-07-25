e52eaaa037ecec8c270844ecc1ca4465b2343d4a
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-07-25 14:31:55 +0630

    Editor and Colorpicker

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
+
+import { Header } from '../components';
+
+const change = (args) => {
+  document.getElementById('preview').style.backgroundColor=args.currentValue.hex;
+}
 
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+    <div className="p-2 m-2 mt-24 bg-white md:m-10 md:p-10 rounded-3xl">
+      <Header category="App" title="Color Picker" />
+      <div className='text-center'>
+        <div id='preview' />
+          <div className='flex flex-wrap items-center justify-center gap-20'>
+            <div>
+              <p className="mt-2 mb-4 text-2xl font-semibold">
+                Inline Pallete
+              </p>
+              <ColorPickerComponent 
+                id="inline-pallete"
+                mode="Palette"
+                modeSwitcher={false}
+                inline
+                showButtons={false}
+                change={change}
+              />
+            </div>
+            <div>
+              <p className="mt-2 mb-4 text-2xl font-semibold">
+                Inline Picker
+              </p>
+              <ColorPickerComponent 
+                id="inline-pallete"
+                mode="picker"
+                modeSwitcher={false}
+                inline
+                showButtons={false}
+                change={change}
+              />
+            </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
